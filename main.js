b7f54dee0e44913676d82075b1849d0e283c0fc7
T-Damer <dpancov64@gmail.com>
2021-08-17 10:25:32 +0300

    Added random stars

--- main.js
+++ main.js
@@ -44,6 +44,27 @@ const controls = new OrbitControls(
   renderer.domElement
 )
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(
+    Math.random(),
+    24,
+    24
+  )
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+  })
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
 function animate() {
   requestAnimationFrame(animate)
 
